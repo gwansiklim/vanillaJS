be8f16744da5c3c8bd96e26b7ce1665783f22415
gwansik <rhkstlr00@gmail.com>
2021-11-10 16:50:23 +0900

    function return 공부

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-const player = {
-    name: "gwansik",
-    point: 10,
-    fat: true
-};
+// const calcultor = {
+//     add: function (a, b) {
+//         console.log(a + b);
+//     },
+//     min: function (a, b) {
+//         console.log(a - b);
+//     },
+//     divi: function (a, b) {
+//         console.log(a / b);
+//     },
+//     squa: function (a, b) {
+//         console.log(a ** b);
+//     }
+// }
+// calcultor.add(1, 2);
+// calcultor.min(1, 2);
+// calcultor.divi(1, 2);
+// calcultor.squa(1, 2);
 
-console.log(player);
-console.log(player.name);
+const calcultor = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    min: function (a, b) {
+        return a - b;
+    },
+    divi: function (a, b) {
+        return a / b;
+    },
+    squa: function (a, b) {
+        return a ** b;
+    }
+};
+const plusResult = calcultor.plus(5, 6);
+console.log(plusResult);
+const minResult = calcultor.min(plusResult, 10)
+console.log(minResult);
+const diviResult = calcultor.divi(plusResult, minResult);
+console.log(diviResult);
