dcb834684e6439c34009ae54d2e91a496782b7a9
gwansik <rhkstlr00@gmail.com>
2021-11-13 20:54:38 +0900

    js- isNaN과 if문

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // function 기초 공부
 
+//boolean은 참인지 거짓인지를 말한다.
+
 // const calcultor = {
 //     add: function (a, b) {
 //         console.log(a + b);
@@ -45,12 +47,23 @@
 
 // string을 숫자로 변환
 
-const age = prompt("How old are you?") // 브라우저창에서 alert창이 생기면서 글자를 쓸수 있게 해준다.
+// 브라우저창에서 alert창이 생기면서 글자를 쓸수 있게 해준다.
+const age = prompt("How old are you?")
 
 //typeof 뒤에 괄호를 넣지 않고 바로 변수명을 사용, typeof는 답을 썼을때 string인지 number인지 알려준다
 // parseInt()는 string을 숫자로 변환 해준다.
-console.log(age)
-console.log(typeof "15", typeof parseInt("hi"))
+// console.log(age)
+// console.log(typeof "15", typeof parseInt("hi"))
 
 // 다른 방식- 질문에 숫자를 입력하면 숫자를 나타나게 해주고 문자를 사용하면 NaN이 뜸.!
-const age = parseInt(prompt("How old are you?"));
+// const age = parseInt(prompt("How old are you?"));
+
+// isNaN을 사용하면 참인지 거짓인지 콘솔에 알려줌
+// console.log(isNaN(age));
+
+//조건문.!
+if (isNaN(age)) {
+    console.log("숫자를 입력해 주세요!!!");
+} else {
+    console.log("감사합니다.!!");
+}
