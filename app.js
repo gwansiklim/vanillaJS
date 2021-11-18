fcf21aab21aec896e1c1ab1c65be96ec930c0315
gwansik <rhkstlr00@gmail.com>
2021-11-18 19:43:57 +0900

    addEventListener 사용하기!

--- app.js
+++ app.js
@@ -12,6 +12,27 @@
 
 //querySelector 자식요소를 불러오는 방법, querySelectorAll은 전부 불러온다.
 
-const title = document.querySelector(".hello h1") //부모의 class를 css쳐럼 가오는게 중요 (여러개가 있을 경우 첫번째 것만 가져온다.)
+// const title = document.querySelector(".hello h1") //부모의 class를 css쳐럼 가오는게 중요 (여러개가 있을 경우 첫번째 것만 가져온다.)
+// console.log(title)
+// title.innerText = "Hello"
 
-console.log(title)
+// 글짜를 클릭했을 시 변화
+const title = document.querySelector("div.hello:first-child h1")
+
+function handleTitleClick() {
+    console.log("change color"); // 브라우저 콘솔에서 확인
+    title.style.color = "blue";
+}
+
+function handleMouseenter() {
+    console.log("mouse is here"); // 브라우저 콘솔에서 확인
+    title.innerText = "Mouse is here";
+}
+
+function handelMouseLeave() {
+    title.innerText = "Mouse is gone";
+}
+
+title.addEventListener("click", handleTitleClick) //마우스로 클릭시 변형
+title.addEventListener("mouseenter", handleMouseenter); //마우스를 올려놓기만 해도 변형 
+title.addEventListener("mouseleave", handelMouseLeave); //마우스를 안올려 놓았을 때 변형!
