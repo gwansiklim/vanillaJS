642e59c050e429ab10d35a620b1135497e441517
gwansik <rhkstlr00@gmail.com>
2021-11-21 22:27:15 +0900

    백틱과 submit 사용

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault(); //function을 사용하면 해당 이벤트를 바로 발생하는 것을 멈추게 해준다(즉, 기본동작을 막아준다.!)
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username} Wellcome`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
