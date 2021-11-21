d3633a721dd225b2532bb2dbe5f958d0fddf097e
gwansik <rhkstlr00@gmail.com>
2021-11-22 00:11:42 +0900

    local DB 정보가져와 화면에 띄우기

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault(); //function을 사용하면 해당 이벤트를 바로 발생하는 것을 멈추게 해준다(즉, 기본동작을 막아준다.!)
-    loginForm.classList.add(HIDDEN_CLASSNAME)
-    const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username} Wellcome`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.classList.add(HIDDEN_CLASSNAME); //css에 있는 .hidden을 적용시킨다.
+    const username = loginInput.value; //username이라는 변수를 이용하여 정보 가져오는 방법!
+    localStorage.setItem(USERNAME_KEY, username); // 웹 디비에 정보 저장!
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username} Wellcome`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the greetings
+    paintGreetings(savedUsername);
+}
