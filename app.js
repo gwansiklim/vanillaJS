fe7344d1e85f90e36a0892f915a7a2506c106e59
gwansik <rhkstlr00@gmail.com>
2021-11-21 23:47:52 +0900

    setItem을 이용하여 localStorage에 정보저장하기

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
     event.preventDefault(); //function을 사용하면 해당 이벤트를 바로 발생하는 것을 멈추게 해준다(즉, 기본동작을 막아준다.!)
     loginForm.classList.add(HIDDEN_CLASSNAME)
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     greeting.innerText = `Hello ${username} Wellcome`;
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
