d170521649f370ad1f1766dcc11b801c2a17e85e
gwansik <rhkstlr00@gmail.com>
2021-11-23 00:47:35 +0900

    실시간 시간 알려주는 방법

---
+++ js/clock.js
@@ -0,0 +1,13 @@
+// 실시간 시간 만드는법
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); //브라우저 키자마자 시간을 바로 나타나게 해준다.!
+setInterval(getClock, 1000); //setInterval(sayHello, 몇초) 인수를 불러오고 단위를 입력해준다.!
