fd0e6e426555e129ba64c1c031499cc6a2cdaad5
gwansik <rhkstlr00@gmail.com>
2021-11-22 10:23:15 +0900

    getDate(),getHours 등등 공부

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+getClock(); //브라우저 키자마자 시간을 바로 나타나게 해준다.!
+setInterval(getClock, 1000); //setInterval(sayHello, 몇초) 인수를 불러오고 단위를 입력해준다.!
