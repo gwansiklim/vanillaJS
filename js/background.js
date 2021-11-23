ea062ccd80e1e69711751f9b6f48599a413697ed
gwansik <rhkstlr00@gmail.com>
2021-11-24 00:22:35 +0900

    todo list 만들기,1

--- js/background.js
+++ js/background.js
@@ -5,8 +5,8 @@ const images = [
 ]
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const bgImage = document.createElement("img"); //html에 <img>를 생성한다.
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`; //html에 <img src="./img/random"> 을 생성.
 
 document.body.appendChild(bgImage);
